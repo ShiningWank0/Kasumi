91d9a87152a5131dac99b8bd96c72acfcbd95f47
ShiningWank0 <174163381+ShiningWank0@users.noreply.github.com>
2026-03-25 17:21:24 +0900

    Initial Commit

---
+++ Kasumi/KasumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KasumiApp.swift
+//  Kasumi
+//
+//  Created by Takuma Kaneko on 2026/03/25.
+//
+
+import SwiftUI
+
+@main
+struct KasumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KasumiTests/KasumiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KasumiTests.swift
+//  KasumiTests
+//
+//  Created by Takuma Kaneko on 2026/03/25.
+//
+
+import Testing
+@testable import Kasumi
+
+struct KasumiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
